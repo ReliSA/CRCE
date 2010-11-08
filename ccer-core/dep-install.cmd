050d5eef2a60b651adbf3676048a1663c75981b5
kalwi <jiri.kucera@kalwi.eu>
2010-11-08 13:56:59 +0000

    Update script for installing underlying SNAPSHOT dependencies.

--- ccer-core/dep-install.cmd
+++ ccer-core/dep-install.cmd
@@ -1,4 +1,4 @@
-cmd /c mvn install:install-file -DgroupId=org.apache.felix -DartifactId=org.apache.felix.dependencymanager -Dversion=3.0.0 -Dpackaging=jar -Dfile=runner/bundles/org.apache.felix.dependencymanager_3.0.0.jar
-cmd /c mvn install:install-file -DgroupId=org.apache.ace -DartifactId=ace-obr-metadata -Dversion=0.8.0 -Dpackaging=jar -Dfile=runner/bundles/org.apache.ace.obr.metadata_0.8.0.jar
-cmd /c mvn install:install-file -DgroupId=org.apache.ace -DartifactId=ace-obr-storage -Dversion=0.8.0 -Dpackaging=jar -Dfile=runner/bundles/org.apache.ace.obr.storage_0.8.0.jar
+cmd /c mvn install:install-file -DgroupId=org.apache.felix -DartifactId=org.apache.felix.dependencymanager -Dversion=3.0.0 -Dpackaging=jar -Dfile=runner/bundles/org.apache.felix.dependencymanager_3.0.0.SNAPSHOT.jar
+cmd /c mvn install:install-file -DgroupId=org.apache.ace -DartifactId=ace-obr-metadata -Dversion=0.8.0 -Dpackaging=jar -Dfile=runner/bundles/org.apache.ace.obr.metadata_0.8.0.SNAPSHOT.jar
+cmd /c mvn install:install-file -DgroupId=org.apache.ace -DartifactId=ace-obr-storage -Dversion=0.8.0 -Dpackaging=jar -Dfile=runner/bundles/org.apache.ace.obr.storage_0.8.0.SNAPSHOT.jar
 
