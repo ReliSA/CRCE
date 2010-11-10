71fa76e7c2c2c4cab7430a60d9e285c5eb4f541b
kalwi <jiri.kucera@kalwi.eu>
2010-11-10 06:51:30 +0000

    Add core bundle.

---
+++ cz.zcu.kiv.ccer/src/main/java/cz/zcu/kiv/ccer/internal/Activator.java
@@ -0,0 +1,23 @@
+package cz.zcu.kiv.ccer.internal;
+
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
+import org.osgi.framework.BundleContext;
+
+/**
+ *
+ * @author kalwi
+ */
+public class Activator extends DependencyActivatorBase {
+
+    @Override
+    public void init(BundleContext bc, DependencyManager dm) throws Exception {
+        
+    }
+
+    @Override
+    public void destroy(BundleContext bc, DependencyManager dm) throws Exception {
+        
+    }
+
+}
