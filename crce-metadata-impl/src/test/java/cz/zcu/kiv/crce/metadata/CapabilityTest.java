9f162dd8f6c7ea59849e00819bf762f37b008c75
kalwi <jiri.kucera@kalwi.eu>
2011-03-26 14:06:12 +0000

    Changes in ResultsStore and Executable interfaces reflecting results of meeting on 2011-03-24.

--- crce-metadata-impl/src/test/java/cz/zcu/kiv/crce/metadata/CapabilityTest.java
+++ crce-metadata-impl/src/test/java/cz/zcu/kiv/crce/metadata/CapabilityTest.java
@@ -1,6 +1,9 @@
 package cz.zcu.kiv.crce.metadata;
 
+import cz.zcu.kiv.crce.metadata.internal.CapabilityImpl;
 import cz.zcu.kiv.crce.metadata.internal.ResourceCreatorImpl;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.*;
 
 /**
@@ -26,7 +29,7 @@ public class CapabilityTest {
     }
 
     @Test
-    public void unsetProperty() {
+    public void unsetProperty() throws Exception {
         Capability c = rc.createCapability("a").setProperty("p1", "1").setProperty("p2", 2);
         
         assert c.getProperty("p1") != null;
@@ -41,7 +44,54 @@ public class CapabilityTest {
         
         assert c.getProperty("p1") == null;
         assert c.getProperty("p2") == null;
+    }
+    
+    @Test
+    public void uniqueProperties() throws Exception {
+        Capability c1 = rc.createCapability("a");
         
+        c1.setProperty("a", "a");
+        assert c1.getProperties().length == 1;
+        
+        c1.setProperty("a", "a");
+        assert c1.getProperties().length == 1;
+        
+        c1.setProperty("a", "b");
+        assert c1.getProperties().length == 1;
 
+        c1.setProperty("b", "a");
+        assert c1.getProperties().length == 2;
+    }
+    
+//    @Test
+//    public void equals() throws Exception {
+//        Capability c1 = rc.createCapability("a");
+//        Capability c2 = rc.createCapability("a");
+//        
+//        assert c1.equals(c2);
+//        
+//        c1.setProperty("p1", "p1");
+//        assert !c1.equals(c2);
+//        
+//        c2.setProperty("p1", "p1");
+//        assert c1.equals(c2);
+//        
+//        
+//        
+//        Set<Capability> cs = new HashSet<Capability>();
+//        
+//        cs.add(c1);
+//        
+//        assert cs.contains(c1);
+//        assert !cs.contains(c2);
+//        
+//        c1.setProperty("p1", "p1");
+//        
+//        assert cs.contains(c1);
+//    }
+    
+    @Test
+    public void hash() throws Exception {
+        
     }
 }
