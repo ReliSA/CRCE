389cd484895689043672807a593b4315fb146fca
Sandcrew <noemail@all.eu>
2011-05-17 19:09:55 +0000

    Feature 1913 - Check

--- crce-webui/src/main/java/cz/zcu/kiv/crce/webui/internal/CheckServlet.java
+++ crce-webui/src/main/java/cz/zcu/kiv/crce/webui/internal/CheckServlet.java
@@ -1,6 +1,7 @@
 package cz.zcu.kiv.crce.webui.internal;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -22,10 +23,12 @@ import cz.zcu.kiv.crce.webui.custom.ResourceExt;
 public class CheckServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		System.out.println(req.getSession().getAttribute("source"));
+			throws ServletException, IOException {		
 		Resource[] res = chooseFrom(req);
-		if(res==null) req.getRequestDispatcher("resource").forward(req, resp);
+		if(res==null)
+		{			
+			req.getRequestDispatcher("resource").forward(req, resp);
+		}
 		else
 		{
 			String source = (String) req.getSession().getAttribute("source");
@@ -36,42 +39,39 @@ public class CheckServlet extends HttpServlet {
 
 	}
 	private Resource[] chooseFrom(HttpServletRequest req){
-		String source = (String )req.getSession().getAttribute("source");
-		System.out.println(source+"choose");
+		String source = (String )req.getSession().getAttribute("source");		
 		if(source == null) return null;
 		else if(source.equals("buffer")) return doCheck(Activator.instance().getBuffer(req).getRepository());
 		else if(source.equals("store")) return doCheck(Activator.instance().getStore().getRepository());
 		else return null;
 	}
-	private Resource[] doCheck(Repository repository) {
-		System.out.println(repository.getName());
+	private Resource[] doCheck(Repository repository) {		
 		Resource[] resources = repository.getResources();
 		ArrayList<Resource> ext = new ArrayList<Resource>();
+		HashMap<URI,Resource> extMap = new HashMap<URI,Resource>();
 		ResourceCreator rc = Activator.instance().getCreator();
 		Resolver resolver = rc.createResolver(repository);
 		for (Resource r : resources) 
 		{
 			resolver.add(r);
+			r.getUri();
+			extMap.put(r.getUri(), new ResourceExt(r));
 			ext.add(new ResourceExt(r));
-		}
+		}		
 		if (!resolver.resolve()) 
-		{
+		{			
 			for (Reason r : resolver.getUnsatisfiedRequirements()) 
-			{
-				if(ext.indexOf(r.getResource())>0)
+			{				
+				if(extMap.containsKey(r.getResource().getUri()))
 				{
-					ext.get(ext.indexOf(r.getResource())).addRequirement(r.getRequirement());
-					System.out.println("found");
+					extMap.get(r.getResource().getUri()).addRequirement(r.getRequirement());					
 				}
 				
-			}
-			System.out.println(ext.size());
-			return ext.toArray(new Resource[ext.size()]);
-
+			}			
+			return extMap.values().toArray(new Resource[extMap.values().size()]);
 		}
 		else
-		{
-			System.out.println("noproblemo");
+		{			
 			return resources;
 		}
 	}
