9f2a5cb308c7d68e03774368f741751e91dd371c
reznicek <reznicek@students.zcu.cz>
2012-08-29 07:24:15 +0000

    Add tag tab functionality

--- crce-webui/src/main/java/cz/zcu/kiv/crce/webui/internal/ResourceServlet.java
+++ crce-webui/src/main/java/cz/zcu/kiv/crce/webui/internal/ResourceServlet.java
@@ -1,6 +1,10 @@
 package cz.zcu.kiv.crce.webui.internal;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +17,7 @@ import org.osgi.service.log.LogService;
 
 import cz.zcu.kiv.crce.metadata.Resource;
 import cz.zcu.kiv.crce.plugin.Plugin;
+import cz.zcu.kiv.crce.webui.internal.bean.Category;
 import org.osgi.framework.InvalidSyntaxException;
 
 public class ResourceServlet extends HttpServlet {
@@ -133,6 +138,36 @@ public class ResourceServlet extends HttpServlet {
 		
 		else if(link.equals("tags"))
 		{
+			Resource[] resources;
+			
+			if(filter==null) {
+				resources = Activator.instance().getStore().getRepository().getResources();
+			} else {
+				try {
+					resources = Activator.instance().getStore().getRepository().getResources(filter);
+				} catch (InvalidSyntaxException e) {
+					setError(session,false,errorMessage);
+					resources = Activator.instance().getStore().getRepository().getResources();
+				}
+			}
+		
+			ArrayList<Category> categoryList = prepareCategoryList(resources);
+			
+			ArrayList<Resource> filteredResourceList;
+		
+			String selectedCategory;
+			if (req.getParameter("tag") != null && req.getParameter("tag") instanceof String) 
+			{
+				selectedCategory = req.getParameter("tag");
+
+				filteredResourceList = filterResurces(selectedCategory, resources);
+				
+			} else {
+				filteredResourceList = null;
+			}
+			
+			session.setAttribute("resources", filteredResourceList);
+			session.setAttribute("categoryList", categoryList);
 			
 			return true;
 		}
@@ -144,4 +179,70 @@ public class ResourceServlet extends HttpServlet {
 		}
 	}
 	
+
+	/**
+	 * Prepare list of ccategories (tags) that are on all resurces in the store.
+	 * Category is represented by name and the number of occurrences.
+	 * @param resources - all resources from the store
+	 * @return array list of categories
+	 */
+	private ArrayList<Category> prepareCategoryList(Resource[] resources) {
+		
+		HashMap<String, Integer> categoryMap = new HashMap<String, Integer>();
+		
+		for(Resource resource: resources) {
+			String[] categories = resource.getCategories();
+			for(String category : categories) {
+				if(categoryMap.containsKey(category)){
+					//category is allready contained, increase count
+					categoryMap.put(category, new Integer(categoryMap.get(category).intValue() + 1));
+				} else {
+					//add new category
+					categoryMap.put(category, new Integer(1));
+				}
+			}
+		}
+		
+		ArrayList<Category> categoryList = new ArrayList<Category>();
+		Set<String> categorySet = categoryMap.keySet();
+		
+		/*Get categories from map to list*/
+		for(String category: categorySet) {
+			Category newCategory = new Category(category);
+			newCategory.setCount(categoryMap.get(category));
+			categoryList.add(newCategory);
+		}
+		
+		//sort category list
+		Collections.sort(categoryList);
+		
+		return categoryList;
+		
+		
+	}
+
+	/**
+	 * Filter resources according some category.
+	 * Output list contains only resources that have required category.
+	 * @param filterCategory required category
+	 * @param resources resources
+	 * @return filtered resources list
+	 */
+	private ArrayList<Resource> filterResurces(String filterCategory, Resource[] resources) {
+		ArrayList<Resource> filteredResourceList = new ArrayList<Resource>();
+		for(Resource resource: resources) {
+			String[] categories = resource.getCategories();
+			for(String category : categories) {
+				if(category.equals(filterCategory)) {
+					filteredResourceList.add(resource);
+					break;
+				}
+			}
+		}
+		
+		return filteredResourceList;
+	}
+
+
+
 }
--- crce-webui/src/main/webapp/jsp/include/footer.jsp
+++ crce-webui/src/main/webapp/jsp/include/footer.jsp
@@ -2,6 +2,6 @@
   
   			<div id="paticka">&copy; 2011-2012 University of West Bohemia, Department of Computer Science</div>
 
-  		</div>
+  		
 	</body>
 </html>
