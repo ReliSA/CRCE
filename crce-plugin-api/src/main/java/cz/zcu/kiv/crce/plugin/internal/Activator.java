4ba26bf60b5c4587e4ced51c0114f95a66311957
kalwi <jiri.kucera@kalwi.eu>
2013-03-31 16:38:29 +0000

    re #31 Copy Plugin API module to incubator.

---
+++ crce-plugin-api/src/main/java/cz/zcu/kiv/crce/plugin/internal/Activator.java
@@ -0,0 +1,45 @@
+package cz.zcu.kiv.crce.plugin.internal;
+
+import cz.zcu.kiv.crce.plugin.MetadataIndexingResultService;
+import cz.zcu.kiv.crce.plugin.Plugin;
+import cz.zcu.kiv.crce.plugin.PluginManager;
+        
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.event.EventAdmin;
+
+/**
+ *
+ * @author Jiri Kucera (kalwi@students.zcu.cz, jiri.kucera@kalwi.eu)
+ */
+public class Activator extends DependencyActivatorBase {
+
+    static final PluginManagerImpl pm = new PluginManagerImpl();
+    
+    /** This instance of MetadataIndexingResultService implementation provides by simple way information
+     * about result of EFP indexing process in crce-efp-indexer module. */
+    private MetadataIndexingResultService efpIndexerResult = new MetadataIndexingResultServiceImpl();
+    
+    @Override
+    public void init(BundleContext context, DependencyManager manager) throws Exception {
+        
+        manager.add(createComponent()
+                .setInterface(PluginManager.class.getName(), null)
+                .setImplementation(pm)
+                .add(createServiceDependency().setRequired(true).setService(EventAdmin.class))
+                .add(createServiceDependency().setRequired(false).setCallbacks("register", "unregister").setService(Plugin.class))
+                );
+        
+		manager.add(createComponent()
+				.setInterface(MetadataIndexingResultService.class.getName(), null)
+				.setImplementation(efpIndexerResult)
+				);
+    }
+
+    @Override
+    public void destroy(BundleContext context, DependencyManager manager) throws Exception {
+        // nothing yet
+    }
+    
+}
---
+++ crce-plugin-api/src/main/java/cz/zcu/kiv/crce/plugin/internal/MetadataIndexingResultServiceImpl.java
@@ -0,0 +1,68 @@
+package cz.zcu.kiv.crce.plugin.internal;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+
+import cz.zcu.kiv.crce.plugin.MetadataIndexingResultService;
+
+
+/**
+ * Implementation of MetadataIndexingResultService interface.
+ */
+public class MetadataIndexingResultServiceImpl implements MetadataIndexingResultService, ManagedService {
+
+    /** This instance holds information about result of indexing process. */
+    private ArrayList<String> messages;
+
+    /** Constructor of MetadataIndexingResultServiceImpl class. */
+    public MetadataIndexingResultServiceImpl() {
+        messages = new ArrayList<String>();
+    }
+
+    /**
+     * Getting metadata indexing result information.
+     *
+     * @return Information about indexing result in String array. Null value when there is no information.
+     */
+    @Override
+    public final String[] getMessages() {
+        if (!isEmpty()) {
+            String[] stringArray = new String[messages.size()];
+            int index = 0;
+            for (String indexerMessage : messages) {
+                stringArray[index++] = indexerMessage;
+            }
+            return stringArray;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Setting information about indexing result.
+     *
+     * @param nextMessage - Information about indexing result.
+     */
+    @Override
+    public final void addMessage(final String nextMessage) {
+        messages.add(nextMessage);
+    }
+
+    @Override
+    public final void removeAllMessages() {
+        messages.clear();
+    }
+
+    @Override
+    public final boolean isEmpty() {
+        return messages.isEmpty();
+    }
+
+    @Override
+    public final void updated(final Dictionary properties) throws ConfigurationException {
+        return;
+    }
+}
