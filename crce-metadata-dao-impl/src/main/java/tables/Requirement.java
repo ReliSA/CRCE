9f12450f73e906ba41a5bde02c131d8b4bc9650f
pcihlar <noemail@all.eu>
2013-06-10 11:05:26 +0000

    re #40: Just attempt. Not tested.

--- crce-metadata-dao-impl/src/main/java/tables/Requirement.java
+++ crce-metadata-dao-impl/src/main/java/tables/Requirement.java
@@ -12,30 +12,38 @@ public class Requirement {
     private String namespace;
     private int requirement_id;
     private int resource_id;
-    
+
     public String getId() {
-        return this.id;
-    }   
-    public String getNamespace() {
-        return this.namespace;
-    }
-    public int getRequirement_id() {
-        return this.requirement_id;
-    }   
-    public int getResource_id() {
-        return this.resource_id;
+        return id;
     }
 
     public void setId(String id) {
         this.id = id;
     }
-    public void setNamespace(String name) {
-        this.namespace = name;
+
+    public String getNamespace() {
+        return namespace;
     }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+
+    public int getRequirement_id() {
+        return requirement_id;
+    }
+
     public void setRequirement_id(int requirement_id) {
         this.requirement_id = requirement_id;
     }
-    public void setNamespace(int resource_id) {
+
+    public int getResource_id() {
+        return resource_id;
+    }
+
+    public void setResource_id(int resource_id) {
         this.resource_id = resource_id;
     }
+    
+
 }
