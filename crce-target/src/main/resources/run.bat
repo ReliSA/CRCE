5bc198df7e4601340fa5ffd80483aa48cb1671f6
kalwi <jiri.kucera@kalwi.eu>
2014-03-12 21:04:35 +0000

    re #73: Run distribution package in sandbox + copy conf directory to sandbox on the first run.

--- crce-target/src/main/resources/run.bat
+++ crce-target/src/main/resources/run.bat
@@ -1,2 +1,8 @@
-@echo off
-call java -jar pax-runner.jar --ups --workingDirectory=. scan-dir:required-bundles scan-dir:crce-bundles scan-file:file:platform.properties %1 %2 %3
+@ECHO OFF
+IF EXIST .\sandbox GOTO START
+
+MKDIR .\sandbox\conf
+COPY .\conf .\sandbox\conf
+
+:START
+CALL java -jar pax-runner.jar --ups --workingDirectory=.\sandbox scan-dir:required-bundles scan-dir:crce-bundles scan-file:file:platform.properties %1 %2 %3
--- crce-target/src/main/resources/run.sh
+++ crce-target/src/main/resources/run.sh
@@ -1,1 +1,6 @@
-java -jar pax-runner.jar --ups --workingDirectory=. scan-dir:required-bundles scan-dir:crce-bundles scan-file:file:platform.properties
+#!/bin/bash
+if [ ! -d ./sandbox ]; then
+  mkdir ./sandbox
+  cp -r ./conf ./sandbox
+fi
+java -jar pax-runner.jar --ups --workingDirectory=./sandbox scan-dir:required-bundles scan-dir:crce-bundles scan-file:file:platform.properties
