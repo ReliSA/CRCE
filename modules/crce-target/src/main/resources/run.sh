81257e9e6c3c2dad18877946685bab995b90c0e9
Jakub Danek <danek.ja@gmail.com>
2016-04-10 17:45:48 +0200

    re #98 fixed dependency setup in crce-target for runnable standalone build.

--- modules/crce-target/src/main/resources/run.sh
+++ modules/crce-target/src/main/resources/run.sh
@@ -3,4 +3,4 @@ if [ ! -d ./sandbox ]; then
   mkdir ./sandbox
   cp -r ./conf ./sandbox
 fi
-java -jar pax-runner.jar --ups --workingDirectory=./sandbox scan-dir:required-bundles scan-dir:crce-bundles scan-file:file:platform.properties
+java -jar pax-runner.jar --ups --workingDirectory=./sandbox scan-dir:required-bundles scan-dir:crce-bundles war:file:crce-wars/crce-webui.war scan-bundle:file:crce-wars/crce-rest-v2.war scan-file:file:platform.properties
