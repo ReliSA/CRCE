63042df5a32a44c00dc865627826dfc891120f0a
Zdenek Vales <valesz@students.zcu.cz>
2018-12-18 20:37:11 +0100

    #8: Fully qualified names fixed so pmd is ok.

--- modules/crce-metadata-osgi-bundle/src/main/java/cz/zcu/kiv/crce/metadata/osgi/internal/OsgiManifestBundleIndexer.java
+++ modules/crce-metadata-osgi-bundle/src/main/java/cz/zcu/kiv/crce/metadata/osgi/internal/OsgiManifestBundleIndexer.java
@@ -42,7 +42,6 @@ import cz.zcu.kiv.crce.metadata.namespace.NsCrceIdentity;
 import cz.zcu.kiv.crce.metadata.osgi.namespace.*;
 import cz.zcu.kiv.crce.metadata.service.MetadataService;
 import cz.zcu.kiv.crce.metadata.type.Version;
-import org.apache.felix.utils.manifest.Attribute;
 import org.apache.felix.utils.manifest.Clause;
 import org.apache.felix.utils.manifest.Directive;
 import org.apache.felix.utils.manifest.Parser;
@@ -122,16 +121,16 @@ public class OsgiManifestBundleIndexer extends AbstractResourceIndexer {
             return Collections.emptyList();
         }
 
-        cz.zcu.kiv.crce.metadata.Attribute<String> pn = osgiIdentity.getAttribute(NsOsgiIdentity.ATTRIBUTE__PRESENTATION_NAME);
+        Attribute<String> pn = osgiIdentity.getAttribute(NsOsgiIdentity.ATTRIBUTE__PRESENTATION_NAME);
         if (pn != null) {
             metadataService.setPresentationName(resource, pn.getValue());
         }
-        cz.zcu.kiv.crce.metadata.Attribute<String> sn = osgiIdentity.getAttribute(NsOsgiIdentity.ATTRIBUTE__SYMBOLIC_NAME);
+        Attribute<String> sn = osgiIdentity.getAttribute(NsOsgiIdentity.ATTRIBUTE__SYMBOLIC_NAME);
         if(sn != null) {
             metadataService.setExternalId(resource, sn.getValue());
         }
 
-        cz.zcu.kiv.crce.metadata.Attribute<Version> ver = osgiIdentity.getAttribute(NsOsgiIdentity.ATTRIBUTE__VERSION);
+        Attribute<Version> ver = osgiIdentity.getAttribute(NsOsgiIdentity.ATTRIBUTE__VERSION);
         if(ver != null) {
             Capability identity = metadataService.getIdentity(resource);
             identity.setAttribute(NsCrceIdentity.ATTRIBUTE__VERSION, ver.getValue());
@@ -354,9 +353,9 @@ public class OsgiManifestBundleIndexer extends AbstractResourceIndexer {
     private Capability createServiceCapability(Clause clause) {
         Capability capability = metadataFactory.createCapability(NsOsgiService.NAMESPACE__OSGI_SERVICE);
         capability.setAttribute(NsOsgiService.ATTRIBUTE__NAME, clause.getName());
-        Attribute[] attributes = clause.getAttributes();
+        org.apache.felix.utils.manifest.Attribute[] attributes = clause.getAttributes();
         if (attributes != null) {
-            for (Attribute attribute : attributes) {
+            for (org.apache.felix.utils.manifest.Attribute attribute : attributes) {
                 capability.setAttribute(new SimpleAttributeType<>(attribute.getName(), String.class), attribute.getValue());
             }
         }
@@ -444,7 +443,7 @@ public class OsgiManifestBundleIndexer extends AbstractResourceIndexer {
         Capability capability = metadataFactory.createCapability(NsOsgiPackage.NAMESPACE__OSGI_PACKAGE);
         capability.setAttribute(NsOsgiPackage.ATTRIBUTE__NAME, clause.getName());
         capability.setAttribute(NsOsgiPackage.ATTRIBUTE__VERSION, new Version(getVersion(clause)));
-        Attribute[] attributes = clause.getAttributes();
+        org.apache.felix.utils.manifest.Attribute[] attributes = clause.getAttributes();
         for (int i = 0; attributes != null && i < attributes.length; i++) {
             String key = attributes[i].getName();
             if (!key.equalsIgnoreCase(Constants.PACKAGE_SPECIFICATION_VERSION) && !key.equalsIgnoreCase(Constants.VERSION_ATTRIBUTE)) {
@@ -478,7 +477,7 @@ public class OsgiManifestBundleIndexer extends AbstractResourceIndexer {
     private void createImportFilter(Requirement requirement, AttributeType<String> name, Clause clause) {
         requirement.addAttribute(name, clause.getName());
         appendVersion(requirement, NsOsgiPackage.ATTRIBUTE__VERSION, getVersionRange(clause));
-        Attribute[] attributes = clause.getAttributes();
+        org.apache.felix.utils.manifest.Attribute[] attributes = clause.getAttributes();
         Set<String> attrs = doImportPackageAttributes(requirement, attributes);
 
         // The next code is using the subset operator
@@ -502,7 +501,7 @@ public class OsgiManifestBundleIndexer extends AbstractResourceIndexer {
     }
 
     @SuppressWarnings("deprecation")
-    private Set<String> doImportPackageAttributes(Requirement requirement, Attribute[] attributes) {
+    private Set<String> doImportPackageAttributes(Requirement requirement, org.apache.felix.utils.manifest.Attribute[] attributes) {
         HashSet<String> set = new HashSet<>();
         for (int i = 0; attributes != null && i < attributes.length; i++) {
             String name = attributes[i].getName();
