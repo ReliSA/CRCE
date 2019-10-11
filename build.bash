d6117862c9fff5a3897f0f6202bc56b9b6ce622f
Premek Brada <p.brada@gmail.com>
2019-10-11 16:41:43 +0200

    convenience tweaks in build script

--- build.bash
+++ build.bash
@@ -1,22 +1,35 @@
 #!/bin/bash
 
+#
+# Simple CRCE build script. 
+#
+# HOWTO: call with one parameter, see the if-switch below for values
+# plus default values as set here.
+#
+
 CFG=$1
-BUILD=clean
+BUILD="clean install"
 PARAMS=
 echo "CRCE build type: ${CFG:-(plain)}"
 
 if [ "$CFG" == "notest" ]; then
     PARAMS="-Dmaven.test.skip=true -Dfindbugs.skip=true";
+elif [ "$CFG" == "onlyclean" ]; then
+    BUILD="clean"
 elif [ "$CFG" == "fast" ]; then
-    BUILD=
+    BUILD="install"
     PARAMS="-Dmaven.test.skip=true -Dfindbugs.skip=true";
 fi
+
+
+# Start the machine ...
+
 
 echo $'\n\n\n'; echo "=============================================================="
 echo "Building crce-parent in ./pom"
 echo "#==============================================================\n\n\n"
 cd pom
-mvn $BUILD install $PARAMS
+mvn $BUILD $PARAMS
 retVal=$?
 cd ..
 if [ $retVal -ne 0 ]; then
@@ -30,7 +43,7 @@ echo $'\n\n\n'; echo "==========================================================
 echo "Building shared-build-settings in ./build"
 echo "#==============================================================\n\n\n"
 cd build
-mvn $BUILD install $PARAMS
+mvn $BUILD $PARAMS
 retVal=$?
 cd ..
 if [ $retVal -ne 0 ]; then
@@ -44,7 +57,7 @@ echo $'\n\n\n'; echo "==========================================================
 echo "Building third party libraries in ./third-party"
 echo "#==============================================================\n\n\n"
 cd third-party
-for d in * ; do cd $d ; mvn $BUILD install  $PARAMS; cd .. ; done
+for d in * ; do cd $d ; mvn $BUILD $PARAMS; cd .. ; done
 retVal=$?
 cd ..
 if [ $retVal -ne 0 ]; then
@@ -58,7 +71,7 @@ echo $'\n\n\n'; echo "==========================================================
 echo "Building crce-core-reactor in ./core"
 echo "#==============================================================\n\n\n"
 cd core
-mvn $BUILD install $PARAMS
+mvn $BUILD $PARAMS
 retVal=$?
 cd ..
 if [ $retVal -ne 0 ]; then
@@ -72,7 +85,7 @@ echo $'\n\n\n'; echo "==========================================================
 echo "Building crce-modules-reactor in ./modules"
 echo "#==============================================================\n\n\n"
 cd modules
-mvn $BUILD install $PARAMS
+mvn $BUILD $PARAMS
 retVal=$?
 cd ..
 if [ $retVal -ne 0 ]; then
