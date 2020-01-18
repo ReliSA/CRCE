c0eb59aea9fa52a15a6d3b71bc62cedabc30d519
Vít Mazín <n5ver@seznam.cz>
2020-01-18 10:28:38 +0100

    Import indexer - javadoc of namespaces

--- modules/crce-import-indexer/src/main/java/cz/zcu/kiv/crce/example/indexer/namespace/NsExampleNs.java
+++ modules/crce-import-indexer/src/main/java/cz/zcu/kiv/crce/example/indexer/namespace/NsExampleNs.java
@@ -3,8 +3,15 @@ package cz.zcu.kiv.crce.example.indexer.namespace;
 import cz.zcu.kiv.crce.metadata.AttributeType;
 import cz.zcu.kiv.crce.metadata.impl.SimpleAttributeType;
 
+/**
+ * This is example namespace which saves count of imports in jar. Namespaces helps hierarchically order indexed
+ * information about artifacts. Every artifact which is uploaded into CRCE will be implicitly indexed by file
+ * indexer and these information is saved under crce.identity namespace.
+ */
 public interface NsExampleNs {
+    //Full name of namespace
     String NAMESPACE__EXAMPLE_NS = "crce.example";
 
+    //Attribute for count of imports
     AttributeType<String> ATTRIBUTE__IMPORT_COUNT = new SimpleAttributeType<>("import-count", String.class);
 }
--- modules/crce-import-indexer/src/main/java/cz/zcu/kiv/crce/example/indexer/namespace/NsImportPackage.java
+++ modules/crce-import-indexer/src/main/java/cz/zcu/kiv/crce/example/indexer/namespace/NsImportPackage.java
@@ -5,8 +5,15 @@ import cz.zcu.kiv.crce.metadata.impl.ListAttributeType;
 
 import java.util.List;
 
+/**
+ * This class represents namespace. Namespaces helps hierarchically order indexed information about artifacts.
+ * This namespace gathers information about imported packages in jar. Every artifact which is uploaded into CRCE
+ * will be implicitly indexed by file indexer and these information is saved under crce.identity namespace.
+ */
 public interface NsImportPackage {
+    //Full name of nasmespace
     String NAMESPACE__JAR_IMPORT = "jar.import.package";
 
+    //Attribute which will be saved under this namespace
     AttributeType<List<String>> ATTRIBUTE__IMPORTED_PACKAGES = new ListAttributeType("imported packages");
 }
