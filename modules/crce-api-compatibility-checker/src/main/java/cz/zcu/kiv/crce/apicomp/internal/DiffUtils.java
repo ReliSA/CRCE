561c6de8d5e96d35d16ca69a651936de17370043
Zdenek Vales <zdenek.vales@yoso.fi>
2020-02-07 17:37:24 +0100

    #19: DiffUtils I forgot to commit.

--- modules/crce-api-compatibility-checker/src/main/java/cz/zcu/kiv/crce/apicomp/internal/DiffUtils.java
+++ modules/crce-api-compatibility-checker/src/main/java/cz/zcu/kiv/crce/apicomp/internal/DiffUtils.java
@@ -1,15 +1,26 @@
 package cz.zcu.kiv.crce.apicomp.internal;
 
+import cz.zcu.kiv.crce.apicomp.result.DifferenceAggregation;
 import cz.zcu.kiv.crce.compatibility.Diff;
 import cz.zcu.kiv.crce.compatibility.Difference;
 import cz.zcu.kiv.crce.compatibility.DifferenceLevel;
 import cz.zcu.kiv.crce.compatibility.impl.DefaultDiffImpl;
+
+import java.util.List;
 
 /**
  * Helper class for working with Diffs.
  *
  */
 public class DiffUtils {
+
+    public static Diff createDiff(String name, DifferenceLevel level, List<Diff> childDiffs) {
+        Difference value = DifferenceAggregation.calculateFinalDifferenceFor(childDiffs);
+        Diff d = createDiff(name, level, value);
+        d.addChildren(childDiffs);
+
+        return d;
+    }
 
     public static Diff createDiff(String name, DifferenceLevel level, Difference value) {
         Diff d = new DefaultDiffImpl();
