1b6d8bf11314a4da3d6f4ae1cc8f7abda8ae785c
Tomáš Ballák <ballak.tomas@protonmail.com>
2020-11-01 00:36:43 +0100

    build fixed

--- build-code-docker.sh
+++ build-code-docker.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 docker image build --tag builder:1.0 ./build
-docker run -it --rm --name crce-project -v "$(pwd)":/usr/src/crce -w /usr/src/crce builder:1.0 bash build-code.bash $1
-docker run -it --rm --name crce-project -v "$(pwd)":/usr/src/crce -w /usr/src/crce/deploy builder:1.0 mvn clean pax:directory
-docker run -it --rm --name crce-project -v "$(pwd)":/usr/src/crce -w /usr/src/crce/deploy builder:1.0 bash prepare-bundles.sh 
+docker run -it --rm --name crce-project -v "$(pwd)":/usr/src/crce -w /usr/src/crce builder:1.0 bash build-code.bash $1 && cd ./deploy && mvn clean pax:directory && ./prepare-bundles.sh
 docker-compose up --build
