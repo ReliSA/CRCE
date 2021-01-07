01f69b4d5cce61a21e5e38e24ff397d12804587e
Tomáš Ballák <ballak.tomas@protonmail.com>
2021-01-07 19:07:47 +0100

    hot fix build.sh

--- build.sh
+++ build.sh
@@ -25,8 +25,9 @@ if [ "$1" = "all" ]; then
     docker image build --tag sharedmodules_compiler:1.0 -f ./build/Dockerfile .
     docker image build --tag metadatamodules_compiler:1.0 --build-arg SERVICE_PATH_PREFIX="./services/metadata" -f ./deploy/docker/Dockerfile .
 
-    docker run --name s_modules_compiler --rm -v shared-modules:/shared-modules sharedmodules_compiler:1.0 \
-                                              -v maven-cache:/root/.m2;
+    docker run --name s_modules_compiler --rm -v shared-modules:/shared-modules \
+                                              -v maven-cache:/root/.m2 \
+                                              sharedmodules_compiler:1.0;
 
     docker run --name s_metadata_compiler --rm -v metadata-modules:/home/crce/modules \
                                             -v shared-modules:/home/crce/shared-modules:ro \
